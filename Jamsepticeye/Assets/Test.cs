2e225b5994088575930ee546bddbfedea1b6b422
Omega5x <david.scherp06@gmail.com>
2025-10-01 14:18:59 +0200

    Added empty project scen

---
+++ Jamsepticeye/Assets/Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public string test;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print(test);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(test + test);
+    }
+}
